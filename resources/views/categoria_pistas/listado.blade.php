a8fa84762ef0e38665d62ac9a3d319d99417ac90
Joel Flores <jjjoelcito123@gmail.com>
2021-07-22 14:05:19 -0400

    Se modifico el nombre del formulario de Categorias pistas

--- resources/views/categoria_pistas/listado.blade.php
+++ resources/views/categoria_pistas/listado.blade.php
@@ -13,7 +13,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">FORMULARIO DE RAZA</h5>
+                <h5 class="modal-title" id="exampleModalLabel">FORMULARIO DE CATEGORIA DE PISTA</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <i aria-hidden="true" class="ki ki-close"></i>
                 </button>
