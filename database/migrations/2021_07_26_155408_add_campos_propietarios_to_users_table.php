b3b72c1ba34c25098f0a60979b41ea178a012bba
Joel Flores <jjjoelcito123@gmail.com>
2021-07-26 12:25:01 -0400

    laves foraneas

---
+++ database/migrations/2021_07_26_155408_add_campos_propietarios_to_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCamposPropietariosToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('socio',10)->nullable();
+            $table->string('genero',50)->nullable();
+            $table->unsignedBigInteger('sucursal_id')->nullable()->after('id');
+            $table->foreign('sucursal_id')->references('id')->on('sucursales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(["sucursal_id"]);
+            $table->dropColumn("surcursal_id");
+            $table->dropForeign(["user_id"]);
+            $table->dropColumn("user_id");
+            $table->dropColumn("socio");
+            $table->dropColumn("genero");
+        });
+    }
+}
